04020afda85191476505d5e4dfcf925f4fa8efb0
Dayo <Calikidd86@gmail.com>
2018-04-06 20:18:15 -0700

    Added Marathon class. Completed class project.

--- src/Lessons/Main.java
+++ src/Lessons/Main.java
@@ -7,20 +7,37 @@ public class Main {
     static Scanner reader = new Scanner(System.in);
 
     public static void main(String[] args){
-        while(true) {
-            System.out.println("Please enter your pay rate:  ");
-            double payRate = reader.nextDouble();
-            System.out.println("Please enter the amount of hours worked: ");
-            int hourWorked = reader.nextInt();
-            System.out.println(Pay.calc(payRate, hourWorked));
-            System.out.println("Are you done?");
-            String status = reader.next();
-            if(status.equalsIgnoreCase("yes")){
-                break;
-            }
+//        while(true) {
+//            System.out.println("Please enter your pay rate:  ");
+//            double payRate = reader.nextDouble();
+//            System.out.println("Please enter the amount of hours worked: ");
+//            int hourWorked = reader.nextInt();
+//            System.out.println(Pay.calc(payRate, hourWorked));
+//            System.out.println("Are you done?");
+//            String status = reader.next();
+//            if(status.equalsIgnoreCase("yes")){
+//                break;
+//            }
             // N3.n3(numero);
             // Gravity.gravFall();
+            String[] names = {
+                    "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt", "Alex",
+                    "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
+                    "Aaron", "Kate"
+            };
+            int[] times = {341, 273, 278, 329, 445, 402, 388, 275, 243, 334, 412, 393, 299,
+                           343, 317, 265
+            };
+            int winner = Marathon.winner(times);
+            int second = Marathon.secondPlace(times);
+            System.out.println(second);
+
+            System.out.println("The racer with the fastest time was " + names[winner] +
+                    " with a time of " + times[winner]);
+
+            System.out.println("The racer in second place is " + names[second] + " with a " +
+                    "time of " + times[second]);
         }
     }
 
-}
+
--- src/Lessons/Marathon.java
+++ src/Lessons/Marathon.java
@@ -2,5 +2,43 @@ package Lessons;
 
 public class Marathon {
 
+    public static int winner(int[] raceTimes){
 
+        int winnerIndex = 0;  // The first place winner
+        int winnerTime = raceTimes[0];   // set winner time to value of 0 index
+        int count = 0;     // iterator, used to track winning index
+
+        for(int elements: raceTimes){
+            if(elements < winnerTime){
+                winnerTime = elements;
+                winnerIndex = count;
+            }
+            count++;
+        }
+        return winnerIndex;
+    }
+
+    public static int secondPlace(int[] raceTimes){
+
+        int winnerIndex = winner(raceTimes);       // The index of the lowest item
+        int secondIndex = 0;                           // The index of the second place item
+        int count = 0;                             // iterator to keep track of index
+        int secondPlace = 0;                       // Second lowest time
+
+        if(winnerIndex == 0){
+            secondIndex = 1;                           // The index of the second place item
+        }
+
+        for(int elements: raceTimes){
+            if(elements == raceTimes[winnerIndex]){
+                count++;
+                continue;
+            } else if(secondPlace == 0 || elements < secondPlace) {
+                secondIndex = count;
+                secondPlace = elements;
+            }
+            count++;
+        }
+        return secondIndex;
+    }
 }
