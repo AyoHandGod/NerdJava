1279b9281a2e3846fa978da29c4623dc7575f5dc
Dayo <Calikidd86@gmail.com>
2018-04-19 19:56:30 -0700

    Updated Dice class with appropriate comments. Updated it to fulfill assignment. Reworking Library class.

--- src/Lessons/Dice.java
+++ src/Lessons/Dice.java
@@ -6,19 +6,39 @@ public class Dice {
     private int sides;
     private int face;
 
+    /**
+     * Dice object constructor #1
+     * @param sides : amount of sides dice has
+     */
     Dice(int sides){
         this.sides = sides;
     }
 
+
+    /**
+     * Dice object constructor #2
+     * @param sides : amount of sides dice has
+     * @param face  ; set face value for dice
+     */
     Dice(int sides, int face){
         this.sides = sides;
         this.face = face;
     }
 
+
+    /**
+     * sides instance variable Getter
+     * @return : amount of sides of dice object
+     */
     public int getSides(){
         return sides;
     }
 
+
+    /**
+     * Updates the amount of sides a dice object has
+     * @param nSides : new amount of sides for dice object
+     */
     public void setSides(int nSides){
         while(nSides % 2 != 0){   // check if sides are valid for dice.
             Scanner reader = new Scanner(System.in);  // Scanner that will be used to read updated input
@@ -29,24 +49,68 @@ public class Dice {
         sides = nSides;
     }
 
+
+    /**
+     *  Simulate Dice roll. Updates face value
+     */
     public void roll(){
         face = (int)(Math.random()* sides) + 1;  // update face of dice using random
     }
 
+
+    /**
+     *
+     * @param d1 : A dice object
+     * @param d2 : Another dice object
+     * @param trials : Amount of times to test rolls until snake eyes
+     */
+    public static void snakeAverage(Dice d1, Dice d2, int trials){
+        int cTotal = 0;
+        for (int i = 0; i < trials; i++) {
+            int count = 0;
+
+            // roll the dice, or the last successful snake eyes pair
+            // will stick
+            d1.roll();
+            d2.roll();
+            while (true) {
+                if (d1.getFace() == 1 && d2.getFace() == 1) {
+                    break;
+                }
+                d1.roll();
+                d2.roll();
+                count++;
+            }
+            System.out.println("Rolled " + count + " times before " +
+                    "landing on snake eyes");
+            cTotal += count;
+        }
+        double average = cTotal / trials;
+        System.out.println("The average number of rolls before snake eyes " +
+                "in " + trials + " rolls, is " + average);
+    }
+
+
+    /**
+     *
+     * @return : Dice objects face value. (Face: the side its landed on)
+     */
     public int getFace() {
         return face;
     }
 
-    public static void main(String[] args){
-        Dice d1 = new Dice(6,1);
-        Dice[] dl = new Dice[4];  // array pointer to array of Dice
 
-        for(Dice element: dl){
-            element = new Dice(6); // initialize a dice object pointer in each array index
-            element.setSides(5);
-            element.roll();
-            System.out.println(element.getSides());
-            System.out.println(element.getFace());
-        }
+    public static void main(String[] args){
+
+        // Create two new dice objects. Use both constructor types
+        // just to test.
+        Dice d1 = new Dice(6);
+        Dice d2 = new Dice(6, 3);
+
+        // Roll dice one to generate a face value for it
+        d1.roll();
+
+        // call our snakeAverage method using the two dice.
+        snakeAverage(d1, d2, 5);
     }
 }
--- src/Lessons/Library.java
+++ src/Lessons/Library.java
@@ -1,31 +1,38 @@
 package Lessons;
 
 public class Library {
+
     private Book[] collection;
-    private String address;
-    public static String hours = "Libraries are open daily from 9am to 5p";
-    public static String[] addresses = new String[30];
-    private int addressIndexer;
-    private int bookIndex;
 
-    public Library(String address){
-        this.address = address;
-        addresses[addressIndexer] = address;
-        addressIndexer += 1;
-        collection = new Book[50];
+
+
+    public static void main(String[] args){
+        // Create Two Libraries
+
+
+
+        // Add Four Books to the First Library
+
+
+        // Print Opening hours and the addresses
+
+
+
+        // Try to borrow The Lords of the Rings from both Libraries
+
+
+
+        // Print the titles of all available books from both libraries
+
+
+
+        // Return the Lords of the Rings to the first library
+
+
+
+        // Print the titles of the available from the first library
+
+
+
     }
-
-    public void addBook(Book title){
-        if(bookIndex < 50) {
-            collection[bookIndex] = title;
-            bookIndex += 1;
-        } else {
-            System.out.println("Library is full at this time");
-        }
-    }
-
-    public Book[] getCollection() {
-        return collection;
-    }
-
 }
